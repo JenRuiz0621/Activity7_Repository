3b4dbcc972bbe8db07798f85bece2ec14d6ebb22
Jeniffer Ruiz <jeny.21ph@gmail.com>
2025-06-06 19:25:48 +0000

    Initial commit

---
+++ webapp/controller/MainView.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.acn.training.activity7.controller.MainView", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/acn/training/activity7/test/unit/controller/MainView.controller"
+], function () {
+	"use strict";
+});
